6d8ebbcd942530804608b004570ce455217b1aed
Daniel Tan <dtan@tripadvisor.com>
2018-05-21 15:31:56 +0800

    not perfect but i got plotly graph to work

--- tutorial-p3.py
+++ tutorial-p3.py
@@ -3,21 +3,75 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas_datareader.data as web
+import datetime
 
+
+tickers=['AAPL','TSLA','GOOG','TRIP']
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    #note this is Input from dcc, not from dash.dependencies
-    dcc.Input(id='input', value='Enter something', type='text'),
-    html.Div(id='output')
-    ])
+    html.H1(children='Stock tracker tutorial'),
+
+    dcc.Input(
+        id='input',
+        value='',
+        type='text'
+    ),
+
+    html.Div(id='output-graph')
+    # Static graph
+    # dcc.Graph(
+    #     id='stock-graph',
+    #     figure={
+    #         'data':[
+    #             {'x':df.index,
+    #             'y':df.Close,
+    #             'type':'line',
+    #             'name':stock+str(df.Close)}
+    #         ],
+    #         'layout' : {
+    #             'title':stock
+    #         }
+    #     }
+    # )
+    
+    # dcc.Input(id='input', value='Enter something', type='text'),
+    # html.Div(id='output')
+])
 
 @app.callback(
-    Output(component_id='output',component_property='children'),
+    Output(component_id='output-graph',component_property='children'),
     [Input(component_id='input',component_property='value')]
 )
-def update_value(input_data):
-    return "Input: {}".format(input_data)
+def update_graph(input_data):
+    
+    if input_data=="":
+        return None
+    elif input_data not in tickers:
+        return None
+    else:
+        
+        start=datetime.datetime(2015,1,1)
+        end=datetime.datetime.now()
+        df=web.DataReader(str(input_data),'morningstar',start,end)
+        df.reset_index(inplace=True)
+        df.set_index("Date",inplace=True)
+        df = df.drop("Symbol",axis=1)
+        return dcc.Graph(
+            id='output-graph',
+            figure={
+                'data':[
+                    {'x':df.index,
+                    'y':df.Close,
+                    'type':'line',
+                    'name':input_data}
+                ],
+                'layout' : {
+                    'title':input_data
+                }
+            }
+        )
 
 
 if __name__ == "__main__":
